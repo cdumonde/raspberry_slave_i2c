9f8b1e2b9b5761f46a40fbb77030724026e5cd47
Cedric Dumondelle cedric.dumondelle@live.fr <cedric.dumondelle@live.fr>
2016-06-20 11:04:47 +0200

    Add last register definition and explanations

--- bcm2835_slave_mod.h
+++ bcm2835_slave_mod.h
@@ -43,53 +43,71 @@
 #define BSC_RSR_OE		0x00000001	// Set when RX FIFO is full and a new data is received (Same as BSC_DR_OE)
 
 /*Control register mask*/
-#define BSC_CR_INV_TXF		0x00002000
-#define BSC_CR_HOSTCTRLEN	0x00001000
-#define BSC_CR_TESTFIFO		0x00000800
-#define BSC_CR_INV_RXF		0x00000400
-#define BSC_CR_RXE		0x00000200
-#define BSC_CR_TXE		0x00000100
-#define BSC_CR_BRK		0x00000080
-#define BSC_CR_ENCTRL		0x00000040
-#define BSC_CR_ENSTAT		0x00000020
-#define BSC_CR_CPOL		0x00000010
-#define BSC_CR_CPHA		0x00000008
-#define BSC_CR_I2C		0x00000004
-#define BSC_CR_SPI		0x00000002
-#define BSC_CR_EN		0x00000001
+#define BSC_CR_INV_TXF		0x00002000	// Change the default reset value of TX flags
+#define BSC_CR_HOSTCTRLEN	0x00001000	// Allow the host control
+#define BSC_CR_TESTFIFO		0x00000800	// Allow the TEST FIFO
+#define BSC_CR_INV_RXF		0x00000400	// Change the default reset value of RX flags
+#define BSC_CR_RXE		0x00000200	// Enable receive mode
+#define BSC_CR_TXE		0x00000100	// Enable transmit mode
+#define BSC_CR_BRK		0x00000080	// Stop operation and clear FIFO
+#define BSC_CR_ENCTRL		0x00000040	// Enable Control, ordinary I2C protocol when unset
+#define BSC_CR_ENSTAT		0x00000020	// Enable Status, ordinary I2C protocol when unset
+#define BSC_CR_CPOL		0x00000010	// Clock polarity
+#define BSC_CR_CPHA		0x00000008	// Clock phase
+#define BSC_CR_I2C		0x00000004	// Enable I2C mode
+#define BSC_CR_SPI		0x00000002	// Enable SPI mode
+#define BSC_CR_EN		0x00000001	// Enable I2C/SPI slave
 
 /*Flag register mask*/
-#define BSC_FR_RXFLEVEL		0x0000F800
-#define BSC_FR_TXFLEVEL		0x000007C0
-#define BSC_FR_RXBUSY		0x00000020
-#define BSC_FR_TXFE		0x00000010
-#define BSC_FR_RXFF		0x00000008
-#define BSC_FR_TXFF		0x00000004
-#define BSC_FR_RXFE		0x00000002
-#define BSC_FR_TXBUSY		0x00000001
+#define BSC_FR_RXFLEVEL		0x0000F800	// Return the current level of RX FIFO
+#define BSC_FR_TXFLEVEL		0x000007C0	// Return the current level of TX FIFO
+#define BSC_FR_RXBUSY		0x00000020	// Set when receive operation is in operation
+#define BSC_FR_TXFE		0x00000010	// Set when TX FIFO is empty
+#define BSC_FR_RXFF		0x00000008	// Set when RX FIFO is full
+#define BSC_FR_TXFF		0x00000004	// Set when TX FIFO is full
+#define BSC_FR_RXFE		0x00000002	// Set when RX FIFO is empty
+#define BSC_FR_TXBUSY		0x00000001	// Set when transmit operation is in operation
 
 /*Interrupt FIFO level mask*/
-#define BSC_IFLS_RXIFLSEL	0x00000038
-#define BSC_IFLS_TXIFLSEL	0x00000007
+#define BSC_IFLS_RXIFLSEL	0x00000038	// Mask to select RX interrupt FIFO level
+#define BSC_IFLS_TXIFLSEL	0x00000007	// Mask to select TX interrupt FIFO level
 
 /*Interrupt FIFO level value*/
-#define BSC_IFLS_ONE_EIGHTS	0x00000000
-#define BSC_IFLS_ONE_QUART	0x00000009
-#define BSC_IFLS_ONE_HALF	0x00000012
-#define BSC_IFLS_THREE_QUART	0x0000001B
-#define BSC_IFLS_SEVEN_EIGHTS	0x00000024
+#define BSC_IFLS_ONE_EIGHTS	0x00000000	// Interrupt is set when fifo gets 1/8 full
+#define BSC_IFLS_ONE_QUART	0x00000009	// Interrupt is set when fifo gets 1/4 full
+#define BSC_IFLS_ONE_HALF	0x00000012	// Interrupt is set when fifo gets 1/2 full
+#define BSC_IFLS_THREE_QUART	0x0000001B	// Interrupt is set when fifo gets 3/4 full
+#define BSC_IFLS_SEVEN_EIGHTS	0x00000024	// Interrupt is set when fifo gets 7/8 full
 
 /*Interrupt mask set/clear register mask*/
-#define BSC_IMSC_OEIM		0x00000008
-#define BSC_IMSC_BEIM		0x00000004
-#define BSC_IMSC_TXIM		0x00000002
-#define BSC_IMSC_RXIM		0x00000001
+#define BSC_IMSC_OEIM		0x00000008	// Overrun error interrupt mask
+#define BSC_IMSC_BEIM		0x00000004	// Break error interrupt mask
+#define BSC_IMSC_TXIM		0x00000002	// Transmit interrupt mask
+#define BSC_IMSC_RXIM		0x00000001	// Receive interrupt mask
 
-/* Bitfields in MIS */
-#define BSC_MIS_TXMIS		0x00000002
-#define BSC_MIS_RXMIS		0x00000001
+/*Masked interrupt status register mask*/
+#define BSC_MIS OEMIS		0x00000008	// Overrun error masked interrupt status
+#define BSC_MIS_BEMIS		0x00000004	// Break error masked interrupt status
+#define BSC_MIS_TXMIS		0x00000002	// Transmit masked interrupt status
+#define BSC_MIS_RXMIS		0x00000001	// Receive masked interrupt status
 
-/* Bitfields in ICR */
-#define BSC_ICR_TXIC		0x00000002
+/*Interrupt clear register mask*/
+#define BSC_ICR_OEIC		0x00000008	// Overrun error interrupt clear
+#define BSC_ICR_BEIC		0x00000004	// Break error interrupt clear
+#define BSC_ICR_TXIC		0x00000002	// Transmit interrupt clear
+#define BSC_ICR_RXIC		0x00000001	// Receive interrupt clear
 
+/*Test data register mask*/
+#define BSC_TDR_DATA		0x0000000F	// Test data register, written into the RX FIFO or read out of the TX FIFO
 
+/*GPU status register mask*/
+#define BSC_GPUSTAT_DATA	0x00000007
+
+/*Host control register mask*/
+#define BSC_HCTRL_DATA		0x000000FF
+
+/*I2C debug register mask*/
+#define BSC_DEBUG1_DATA		0x03FFFFFF
+
+/*SPI debug register mask*/
+#define BSC_DEBUG2_DATA		0x00FFFFFF
